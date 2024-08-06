f0a4ff0a98a7450559d272834765fb7164d5c101
hironmoy-ray <hpray35@gmail.com>
2024-08-06 13:57:36 +0600

    Final Done

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ remainingField.innerHTML = 10;
 const resultField = document.querySelector(".result");
 const resultBox = document.querySelector(".result-box");
 const startBtn = document.querySelector(".start");
+const resultH3 = document.querySelector(".result-h3");
 
 let guesses = [];
 let count = 1;
@@ -45,18 +46,21 @@ function setPrevious (guess){
 
 function result(guess){
     if(guess == randomValue){
-        resultField.innerHTML = `Correct. The number is ${guess}`;
+        resultField.innerHTML = `Correct.`;
         console.log(count);
+        resultH3.classList.add('bg-green')
         submit.setAttribute("disabled","");
         startBtn.removeAttribute('disabled')
         newGame();
         
     }else if(guess > randomValue){
         resultField.innerHTML = `too large`;
+        resultH3.classList.add('bg-red')
         console.log(count);
         endGame();
     }
     else if(guess < randomValue){
+        resultH3.classList.add('bg-red')
         resultField.innerHTML = `too small`;
         console.log(count);
         endGame();
@@ -66,23 +70,28 @@ function result(guess){
 function endGame(){
     if(count == 10){
         submit.setAttribute("disabled","");
-        startBtn.removeAttribute('disabled')
+        startBtn.removeAttribute('disabled');
         newGame();
     }
 }
 
 function newGame(){
- startBtn.addEventListener("click",()=>{
-     randomValue = Math.floor(Math.random()*100+1);
-    //  console.log(`Inner: ${randomValue}`);
-     count = 1;
-     guesses=[];
-     previousField.innerHTML = '';
-     remainingField.innerHTML = '';
-     resultField.innerHTML = '';
-     startBtn.setAttribute("disabled","");
-     submit.removeAttribute('disabled');
+    startBtn.addEventListener("click",()=>{
+        randomValue = Math.floor(Math.random()*100+1);
+        count = 1;
+        guesses=[];
+        previousField.innerHTML = '';
+        remainingField.innerHTML = '';
+        resultField.innerHTML = '';
+        startBtn.setAttribute("disabled","");
+        submit.removeAttribute('disabled');
+        
+
+        console.log(startBtn.hasAttribute("disabled"));
+
+     resultH3.classList.remove("bg-green","bg-red");
  })
+ 
 }
 
 
